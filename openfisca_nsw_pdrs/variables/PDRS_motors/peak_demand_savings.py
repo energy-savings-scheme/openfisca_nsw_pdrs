81ee5f969f1674fd8140e92e77a2945f2947c060
YuguangIpsen <yuguang.fan@gmail.com>
2021-03-31 16:24:51 +1100

    Add baseline_motor_efficiency Table + linearInterpolation + dummy period

--- openfisca_nsw_pdrs/variables/PDRS_motors/peak_demand_savings.py
+++ openfisca_nsw_pdrs/variables/PDRS_motors/peak_demand_savings.py
@@ -3,6 +3,7 @@ from openfisca_core.variables import Variable
 from openfisca_core.periods import ETERNITY
 from openfisca_core.indexed_enums import Enum
 from openfisca_nsw_base.entities import Building
+from openfisca_core.parameters import load_parameter_file
 
 
 
@@ -36,8 +37,44 @@ class PDRS__motors__baseline_motor_efficiency(Variable):
     def formula(building, period, parameters):
         rated_output = building('PDRS__motors__rated_output', period)
 
-        baseline_efficiency_table=parameters(period).motors.motors_baseline_efficiency_table
-        return baseline_efficiency_table.calc(rated_output, right=False)
+        #access the thresholds and amounts in parameters
+        data = load_parameter_file("openfisca_nsw_pdrs/parameters/motors/motors_baseline_efficiency_table.yaml")
+
+        output_threshold = []
+        baselineEfficiency = []
+
+        # prison break for parameters
+        for parameterAtInstance in data.children['rated_output'].values_list:
+            output_threshold.append(parameterAtInstance.value)
+
+        for parameterAtInstance in data.children['2poles'].values_list:
+            baselineEfficiency.append(parameterAtInstance.value)
+
+        print(baselineEfficiency)
+
+        #compute gradients
+        delta_y = np.delete(baselineEfficiency, 0) - baselineEfficiency[:-1]
+        delta_x = np.delete(output_threshold,0)-output_threshold[:-1]
+        gradient = delta_y/delta_x
+        x_zip = zip(range(len(output_threshold)-1),output_threshold[:-1], np.delete(output_threshold,0))
+
+        #find out which bracket does the user input belong to
+        condList = []
+
+        for (index, z1, z2) in x_zip:
+            bracket_pos=(rated_output >= z1) * (rated_output < z2)
+            condList.append(bracket_pos)
+
+        x_lowerbound = np.select(np.array(condList), output_threshold[:-1])
+        y_minus = np.select(np.array(condList), baselineEfficiency[:-1])
+        gradient_selected = np.select(np.array(condList), gradient)
+
+
+        return y_minus + gradient_selected*(rated_output-x_lowerbound)
+
+
+
+
 
 
 
@@ -56,7 +93,6 @@ class PDRS__motors__peak_demand_savings(Variable):
 
     def formula(building, period, parameters):
         rated_output = building('PDRS__motors__rated_output', period)
-        print(rated_output)
         asset_life_table=parameters(period).motors.motors_asset_life_table
         forward_creation_period=asset_life_table.calc(rated_output, right=False)
 
